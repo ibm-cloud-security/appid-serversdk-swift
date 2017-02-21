c597613498a597ec951f4ef833fa15f500005a70
Oded Betzalel <ODEDB@il.ibm.com>
2017-02-21 17:56:47 +0200

    updating package swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 let package = Package(
     name: "AppIDServerSDKSwift",
 	dependencies:[
-		.Package(url: "https://github.com/IBM-Swift/Kitura-Credentials.git", majorVersion: 1, minor: 6)
+		.Package(url: "https://github.com/IBM-Swift/Kitura-Credentials.git", majorVersion: 1, minor: 6),
+        .Package(url: "https://github.com/IBM-Swift/Kitura-Request.git", majorVersion: 0)
 	]
 )
