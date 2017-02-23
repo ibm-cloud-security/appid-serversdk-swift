06955f560f82863d8f7f6c8e17a97c61cd268b73
Oded Betzalel <ODEDB@il.ibm.com>
2017-02-23 15:09:25 +0200

    0.8.0

--- Sample/Package.swift
+++ Sample/Package.swift
@@ -3,5 +3,5 @@ import PackageDescription
 let package = Package(
     name: "AppIDUser",
     dependencies: [
-        .Package(url: "https://github.com/ibm-cloud-security/appid-serversdk-swift.git", majorVersion: 0, minor:7)
+        .Package(url: "https://github.com/ibm-cloud-security/appid-serversdk-swift.git", majorVersion: 0, minor:8)
     ])
