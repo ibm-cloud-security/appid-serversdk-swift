27c59cddcafaf1d9bb5270193b71b960a5df2665
Anton Aleksandrov <antona@us.ibm.com>
2017-02-23 16:02:05 -0600

    SDK Rename, added tests

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "AppIDServerSDKSwift",
+    name: "BluemixAppID",
 	dependencies:[
 		.Package(url: "https://github.com/IBM-Swift/Kitura-Credentials.git", majorVersion: 1, minor: 6),
         .Package(url: "https://github.com/IBM-Swift/Kitura-Request.git", majorVersion: 0)
