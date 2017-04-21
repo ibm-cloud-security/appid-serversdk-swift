df5c1b54525003e0bcf54114c05c880eb07c7490
rolivieri <roliv@us.ibm.com>
2017-04-21 11:52:36 -0500

    Updated dependency in Package.swift to fix compilation error.

--- Package.swift
+++ Package.swift
@@ -2,9 +2,9 @@ import PackageDescription
 
 let package = Package(
   name: "BluemixAppID",
-	dependencies:[
-		.Package(url: "https://github.com/IBM-Swift/Kitura-Credentials.git", majorVersion: 1, minor: 6),
+  dependencies:[
+    .Package(url: "https://github.com/IBM-Swift/Kitura-Credentials.git", majorVersion: 1, minor: 6),
     .Package(url: "https://github.com/IBM-Swift/Kitura-Request.git", majorVersion: 0, minor: 7),
-		.Package(url: "https://github.com/ibm-bluemix-mobile-services/bluemix-simple-logger-swift.git", majorVersion: 0, minor: 5)
-	]
+    .Package(url: "https://github.com/ibm-bluemix-mobile-services/bluemix-simple-logger-swift.git", majorVersion: 0, minor: 5)
+  ]
 )
