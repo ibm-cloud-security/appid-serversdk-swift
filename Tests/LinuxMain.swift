c5f0631daf94f196d64d4af46fe8d83ff4458e9c
andy <andrew.lees11@ibm.com>
2018-10-31 13:29:31 +0000

    Test: Ignore tests which fail on 4.0 from linux bug

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,11 +1,23 @@
 import XCTest
 @testable import IBMCloudAppIDTests
 
+#if swift(>=4.1)
 XCTMain([
      testCase(IBMCloudAppIDTests.allTests),
-     testCase(UserProfileManagerTests.allTests),
      testCase(UtilsTest.allTests),
      testCase(WebAppPluginTest.allTests),
+     testCase(AppIDPluginConfigTests.allTests),
+     // The following tests fail on linux 4.0 due to the bug raised here:
+     // https://bugs.swift.org/browse/SR-6968
+     // This has been fixed in versions 4.1 onwards.
+     testCase(UserProfileManagerTests.allTests),
      testCase(ApiPluginTests.allTests),
-     testCase(AppIDPluginConfigTests.allTests)
 ])
+#else
+XCTMain([
+     testCase(IBMCloudAppIDTests.allTests),
+     testCase(UtilsTest.allTests),
+     testCase(WebAppPluginTest.allTests),
+     testCase(AppIDPluginConfigTests.allTests),
+])
+#endif
